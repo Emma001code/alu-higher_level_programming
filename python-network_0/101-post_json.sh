8d6208800bc326724a1f3b2d69ddaf8134ce89dd
Emma001code <e.ngwoke@alustudent.com>
2024-11-28 12:46:55 +0000

    Addy tasks

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# sends a json POST request to a url
-curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2" 
-=======
-# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
->>>>>>> 009d834382a5d75470e05567fcedd99abdfaa3dc
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# makes a request to a file and gets a response message
-curl -sLX PUT -d "user_id=98" -H "origin: HolbertonSchool" "0.0.0.0:5000/catch_me"
-=======
-# Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
->>>>>>> 009d834382a5d75470e05567fcedd99abdfaa3dc
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
