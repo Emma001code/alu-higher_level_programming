3a31e261e634f8f8982c1b35e78556ef2ab7036d
Emma001code <e.ngwoke@alustudent.com>
2025-01-31 09:54:15 -0800

    correction and validation

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv[2];
-if (args === undefined) {
+if (!process.argv[2]) {
   console.log('No argument');
+} else if ((process.argv[2]) && (!process.argv[3])) {
+  console.log('Argument found');
 } else {
-  console.log(args);
+  console.log('Arguments found');
 }
-
